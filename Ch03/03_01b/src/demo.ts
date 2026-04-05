67676b67e9f36ef897bc46271ac03858da754ffa
Jimish Gajjar <jimish.gajjar@gmail.com>
2026-04-05 00:31:39 -0400

    Add TypeScript types and functions for managing contacts and searching functionality

---
+++ Ch03/03_01b/src/demo.ts
@@ -0,0 +1,40 @@
+type ContactName = string;
+
+type ContactStatus = "active" | "inactive" | "new";
+
+type ContactBirthdate = Date | string | number;
+
+interface Contact {
+    id: number;
+    name: ContactName;
+    birthDate?: ContactBirthdate;
+    status?: ContactStatus;
+}
+
+interface Address {
+    line1: string;
+    line2: string;
+    province: string;
+    region: string;
+    postalCode: string;
+}
+
+type AddressableContact = Contact & Address;
+
+function getBirthDate(contact: Contact) {
+    if (typeof contact.birthDate === "number") {
+        return new Date(contact.birthDate);
+    }
+    else if (typeof contact.birthDate === "string") {
+        return Date.parse(contact.birthDate)
+    }
+    else {
+        return contact.birthDate
+    }
+}
+
+let primaryContact: Contact = {
+    id: 12345,
+    name: "Jamie Johnson",
+    status: "active",
+}
